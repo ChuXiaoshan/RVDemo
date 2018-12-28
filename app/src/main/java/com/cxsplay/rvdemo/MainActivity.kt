fe6f71c2e141d80033a38ab50591f125eb288e1c
CxS <xiaoshan1215@gmail.com>
2018-12-28 15:29:12 +0800

    iiI

--- app/src/main/java/com/cxsplay/rvdemo/MainActivity.kt
+++ app/src/main/java/com/cxsplay/rvdemo/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.cxsplay.rvdemo
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -19,6 +20,17 @@ class MainActivity : AppCompatActivity() {
         }
         btn2.setOnClickListener {
             startActivity(Intent(this, PinchViewActivity::class.java))
+        }
+        btn3.setOnClickListener {
+            val content = "pinduoduo://com.xunmeng.pinduoduo/duo_coupon_landing.html?goods_id=580820933&pid=10001_20999"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(content))
+            startActivity(intent)
+        }
+        btn4.setOnClickListener {
+            val content =
+                "openApp.jdMobile://virtual?params={\"category\":\"jump\",\"des\":\"productDetail\",\"skuId\":\"1157957\",\"sourceType\":\"JSHOP_SOURCE_TYPE\",\"sourceValue\":\"JSHOP_SOURCE_VALUE\"}"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(content))
+            startActivity(intent)
         }
     }
 }
