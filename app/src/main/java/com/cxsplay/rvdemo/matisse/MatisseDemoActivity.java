e732d7ef93be35421af11f4cc296e777ce633de4
CxS <xiaoshan1215@gmail.com>
2019-04-02 18:03:17 +0800

    ii

--- app/src/main/java/com/cxsplay/rvdemo/matisse/MatisseDemoActivity.java
+++ app/src/main/java/com/cxsplay/rvdemo/matisse/MatisseDemoActivity.java
@@ -2,17 +2,27 @@ package com.cxsplay.rvdemo.matisse;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import com.blankj.utilcode.util.LogUtils;
 import com.blankj.utilcode.util.ScreenUtils;
 import com.cxsplay.rvdemo.BuildConfig;
 import com.cxsplay.rvdemo.R;
+import com.cxsplay.rvdemo.common.ImageLoader;
 import com.cxsplay.rvdemo.databinding.ActivityMatisseDemoBinding;
+import com.yalantis.ucrop.UCrop;
 import com.zhihu.matisse.Matisse;
 import com.zhihu.matisse.MimeType;
 import com.zhihu.matisse.internal.entity.CaptureStrategy;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MatisseDemoActivity extends AppCompatActivity {
 
@@ -43,6 +53,28 @@ public class MatisseDemoActivity extends AppCompatActivity {
                 .forResult(1);
     }
 
+
+    private String getAppName() {
+        try {
+            PackageManager pm = getPackageManager();
+            PackageInfo pi = pm.getPackageInfo(getApplicationContext().getPackageName(), 0);
+            return pi.applicationInfo.loadLabel(pm).toString();
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return "Pictures";
+        }
+    }
+
+    private File getFile() {
+        if (!"mounted".equals(Environment.getExternalStorageState())) {
+            return null;
+        }
+        String externalStoragePath = Environment.getExternalStorageDirectory().getAbsolutePath();
+        File file = new File(externalStoragePath, getAppName());
+        return !file.exists() && !file.mkdirs() ? null : file;
+    }
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -50,6 +82,42 @@ public class MatisseDemoActivity extends AppCompatActivity {
             Log.d("Matisse", "Uris: " + Matisse.obtainResult(data));
             Log.d("Matisse", "Paths: " + Matisse.obtainPathResult(data));
             Log.e("Matisse", "Use the selected photos with original: " + String.valueOf(Matisse.obtainOriginalState(data)));
+
+
+            File imgFile = new File(getFile(), "hhhh.jpg");
+            try {
+                boolean dd = imgFile.createNewFile();
+                LogUtils.d("---create--->" + dd);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Uri destinationUri;
+//            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            destinationUri = Uri.fromFile(imgFile);
+//            } else {
+//                String authority = getApplicationContext().getPackageName() + ".provider";
+//                destinationUri = FileProvider.getUriForFile(this, authority, imgFile);
+//            }
+
+            LogUtils.d("----->" + destinationUri.getPath());
+            UCrop.of(Matisse.obtainResult(data).get(0), destinationUri)
+                    .withAspectRatio(1, 1)
+                    .withMaxResultSize(200, 200)
+                    .start(this);
+        }
+
+        LogUtils.d("1");
+        LogUtils.d("---resultCode--->" + resultCode);
+        LogUtils.d("---requestCode--->" + requestCode);
+        if (resultCode == RESULT_OK && requestCode == UCrop.REQUEST_CROP) {
+            LogUtils.d("2");
+            final Uri resultUri = UCrop.getOutput(data);
+            String path = resultUri.getPath();
+            LogUtils.d("----path--->" + path);
+            ImageLoader.loadImageByUrl(bind.iv1, path);
+        } else if (resultCode == UCrop.RESULT_ERROR) {
+            final Throwable cropError = UCrop.getError(data);
+            LogUtils.e("---->" + cropError.getMessage());
         }
     }
 }
