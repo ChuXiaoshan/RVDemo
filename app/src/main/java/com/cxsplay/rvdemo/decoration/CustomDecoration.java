6da05e9040c07b0116398d563b235e76b6c7f045
CxS <xiaoshan1215@gmail.com>
2019-04-23 17:57:38 +0800

    ii

--- app/src/main/java/com/cxsplay/rvdemo/decoration/CustomDecoration.java
+++ app/src/main/java/com/cxsplay/rvdemo/decoration/CustomDecoration.java
@@ -1,13 +1,14 @@
 package com.cxsplay.rvdemo.decoration;
 
 import android.content.Context;
-import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import org.jetbrains.annotations.NotNull;
 
 public class CustomDecoration extends RecyclerView.ItemDecoration {
 
@@ -19,29 +20,39 @@ public class CustomDecoration extends RecyclerView.ItemDecoration {
         mDrawable = context.getResources().getDrawable(resId);
     }
 
-    @Override
-    public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
-        int left = parent.getPaddingLeft();
-        int right = parent.getWidth() - parent.getPaddingRight();
-        int childCount = parent.getChildCount();
-        for (int i = 0; i < childCount; i++) {
-            View child = parent.getChildAt(i);
-//            RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
-//            int top = child.getBottom() + params.bottomMargin;
-//            int bottom = top + mDrawable.getIntrinsicHeight();
-//            mDrawable.setBounds(left, top, right, bottom);
-//            mDrawable.draw(c);
+//    @Override
+//    public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+//        c.save();
+//        int left = parent.getPaddingLeft();
+//        int right = parent.getWidth() - parent.getPaddingRight();
+//        int childCount = parent.getChildCount();
+//        paint.setStyle(Paint.Style.FILL);
+//        paint.setColor(Color.TRANSPARENT);
+//        for (int i = 0; i < childCount; i++) {
+//            View child = parent.getChildAt(i);
+////            RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+////            int top = child.getBottom() + params.bottomMargin;
+////            int bottom = top + mDrawable.getIntrinsicHeight();
+////            mDrawable.setBounds(left, top, right, bottom);
+////            mDrawable.draw(c);
+//            this.mBoundsF.set(child.getLeft(), child.getBottom(), child.getRight(), child.getBottom() + 20);
+//            c.drawRoundRect(this.mBoundsF, 1, 1, paint);
+//        }
+//        c.restore();
+//    }
 
-            paint.setStyle(Paint.Style.STROKE);
-            paint.setStrokeWidth(20);
-            this.mBoundsF.set(child.getLeft(), child.getBottom(), child.getRight(), child.getBottom() + 20);
-            c.drawRoundRect(this.mBoundsF, 1, 1, paint);
+
+    @Override
+    public void getItemOffsets(@NotNull Rect outRect, @NotNull View view, @NotNull RecyclerView parent, @NotNull RecyclerView.State state) {
+        RecyclerView.LayoutManager layoutManager = parent.getLayoutManager();
+        if (layoutManager instanceof GridLayoutManager) {
+            GridLayoutManager glm = (GridLayoutManager) layoutManager;
+            int spanCount = glm.getSpanCount();
+            GridLayoutManager.SpanSizeLookup spanSizeLookup = glm.getSpanSizeLookup();
+            int childAdapterPosition = parent.getChildAdapterPosition(view);
+            if (spanCount == spanSizeLookup.getSpanSize(childAdapterPosition)) {
+                outRect.set(0, 10, 0, 0);
+            }
         }
-    }
-
-
-    @Override
-    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
-        outRect.set(0, 0, 0, mDrawable.getIntrinsicWidth());
     }
 }
--- app/src/main/java/com/cxsplay/rvdemo/decoration/List1Activity.java
+++ app/src/main/java/com/cxsplay/rvdemo/decoration/List1Activity.java
@@ -2,8 +2,13 @@ package com.cxsplay.rvdemo.decoration;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.cxsplay.rvdemo.R;
 import com.cxsplay.rvdemo.bean.Simple;
 import com.cxsplay.rvdemo.databinding.ActivityList1Binding;
@@ -45,12 +50,51 @@ public class List1Activity extends AppCompatActivity {
         mList.add(new Simple());
     }
 
+    private BaseQuickAdapter.SpanSizeLookup spanSizeLookup = (gridLayoutManager, position) -> {
+        if (position < 3) {
+            return 1;
+        }
+        return 3;
+    };
+
     private void init() {
         List1Adapter adapter = new List1Adapter(mList);
-        bind.rv.setLayoutManager(new LinearLayoutManager(this));
+        adapter.setSpanSizeLookup(spanSizeLookup);
+        bind.rv.setLayoutManager(new GridLayoutManager(this, 3));
         bind.rv.setAdapter(adapter);
         bind.rv.addItemDecoration(new CustomDecoration(this, R.drawable.recyclerview_item_divider));
 //        bind.rv.addItemDecoration(new SimpleDividerDecoration(10, Color.BLUE));
         adapter.notifyDataSetChanged();
+    }
+
+    public static class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+        @Override
+        public int getItemViewType(int position) {
+            return super.getItemViewType(position);
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+            }
+        }
     }
 }
