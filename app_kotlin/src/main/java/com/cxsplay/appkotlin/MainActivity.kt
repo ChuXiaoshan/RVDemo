9f4acab2b02965e7f1468f4041ed1c216091ca10
CxS <xiaoshan1215@gmail.com>
2019-04-28 17:55:49 +0800

    i

--- app_kotlin/src/main/java/com/cxsplay/appkotlin/MainActivity.kt
+++ app_kotlin/src/main/java/com/cxsplay/appkotlin/MainActivity.kt
@@ -13,13 +13,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun init() {
-        val list = listOf("a", "b", "c")
 
-        if (-1 !in 0..list.lastIndex) {
-            println("-1 is out of range")
-        }
-        if (list.size !in list.indices) {
-            println("list size is out of valid list indices range, too")
-        }
     }
 }
