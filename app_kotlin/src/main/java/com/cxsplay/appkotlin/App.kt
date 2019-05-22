330c161fdd284e91bd1b30ad4f905bc219381b80
CxS <xiaoshan1215@gmail.com>
2019-05-22 11:18:48 +0800

    iiI

--- app_kotlin/src/main/java/com/cxsplay/appkotlin/App.kt
+++ app_kotlin/src/main/java/com/cxsplay/appkotlin/App.kt
@@ -2,7 +2,6 @@ package com.cxsplay.appkotlin
 
 import android.app.Application
 import com.orhanobut.logger.AndroidLogAdapter
-import com.orhanobut.logger.FormatStrategy
 import com.orhanobut.logger.Logger
 import com.orhanobut.logger.PrettyFormatStrategy
 
@@ -19,10 +18,10 @@ class App : Application() {
 
     private fun initLogger() {
         val formatStrategy = PrettyFormatStrategy.newBuilder()
-            .showThreadInfo(true)  // (Optional) Whether to show thread info or not. Default true
+            .showThreadInfo(true)   // (Optional) Whether to show thread info or not. Default true
             .methodCount(1)         // (Optional) How many method line to show. Default 2
             .methodOffset(1)        // (Optional) Hides internal method calls up to offset. Default 5
-            .tag("appkotlin")   // (Optional) Global tag for every log. Default PRETTY_LOGGER
+            .tag("appkotlin")       // (Optional) Global tag for every log. Default PRETTY_LOGGER
             .build()
         Logger.addLogAdapter(object : AndroidLogAdapter(formatStrategy) {
             override fun isLoggable(priority: Int, tag: String?): Boolean {
